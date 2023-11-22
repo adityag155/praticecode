0f94d8ff64548a3fe1c4ce2c82ca219af53428de
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-11-22 11:17:15 +0530

    Add files via upload

---
+++ TestStudent1.java
@@ -0,0 +1,12 @@
+class Student{
+int id;
+String name;
+}
+class TestStudent1{
+public static void main(String args[]){
+Student s1=new Student();
+s1.id=11;
+s1.name="ABC";
+System.out.println(s1.id+" "+s1.name);
+}
+}
