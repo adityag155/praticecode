4be3297ed89e0d587f264e9e4449721ecbe55794
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-07 21:44:56 +0530

    Add files via upload

---
+++ Demo8.java
@@ -0,0 +1,21 @@
+class First
+{
+final int a;
+First()//Assign value through constructor
+{
+a=600;
+}
+}
+class Demo8
+{
+public static void main(String args[])
+{
+First f1=new First();
+System.out.println(f1.a);
+}
+}
+
+/*
+O/P:
+600
+*/
