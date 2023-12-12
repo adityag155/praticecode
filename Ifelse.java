392fd540fb9e05f69d1d91c52e049e09a6e53741
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-12 22:50:54 +0530

    Add files via upload

---
+++ Ifelse.java
@@ -0,0 +1,24 @@
+package adiiapplication;
+
+public class Ifelse {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x=10;
+		int y=12;
+		if(x+y<10) {
+			System.out.println("x+y is less than 10");
+		}
+		else
+		{
+			System.out.println("x+y is greater than 20");
+		}
+
+	}
+
+}
+
+/*
+O/P:
+x+y is greater than 20
+*/
