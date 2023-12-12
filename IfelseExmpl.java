393e1813eb8864a3f8417c97612679ab3dce7bb8
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-12 23:07:24 +0530

    Add files via upload

---
+++ IfelseExmpl.java
@@ -0,0 +1,20 @@
+package adiiapplication;
+
+public class IfelseExmpl {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10;
+		int b=12;
+		if(a+b<30)
+		{
+			System.out.println("a+b is less than 30");
+		}
+		else
+		{
+			System.out.println("a+b is greater than 30");
+		}
+		
+	}
+
+}
