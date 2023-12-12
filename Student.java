e73573792ce7c245b07f4dc7a07f5030085fe796
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-12 22:42:16 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,19 @@
+package adiiapplication;
+
+public class Student {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int x=10;
+int y=12;
+if(x+y>20)
+{
+	System.out.println("x+y is greater than 20");
+	}
+	}
+}
+
+
+/*
+O/P:
+x+y is greater than 20
+*/
