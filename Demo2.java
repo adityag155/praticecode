9d0a089c73151495babd908e62ee66d0e328d05c
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-12 21:40:56 +0530

    Add files via upload

--- Demo2.java
+++ Demo2.java
@@ -1,16 +1,59 @@
-class First
+package adiiapplication;
+
+class Rectangle
 {
-static int a=10;
-}
-class Demo2
-{
-public static void main(String args[])
-{
-System.out.println(First.a);
-}
+	double width,length;
+	Rectangle(double w,double l)
+	{
+		width=w;
+		length=l;
+	}
 }
 
-/*
-O/P:
-10
-*/
+class Triangle
+{
+	double base,height;
+	Triangle(double b,double h)
+	{
+		base=b;
+		height=h;
+	}
+}
+
+class Area
+{
+	double rectangleArea;
+	double triangleArea;
+	Area(double rectArea, double triArea)
+	{
+		rectangleArea=rectArea;
+		triangleArea=triArea;
+	}
+}
+
+class MyLogic
+{
+	Area calculate(Rectangle r, Triangle t)
+	{
+	double rectangleArea=r.width*r.length;
+	double base=0;
+	double triangleArea=0.5*base*t.height;
+	Area a=new Area(rectangleArea,triangleArea);
+	return a;
+	}
+}
+
+public class Demo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+Rectangle rect=new Rectangle(10,20);
+Triangle t=new Triangle(3,40);
+
+MyLogic ml=new MyLogic();
+Area a=ml.calculate(rect, t);
+
+System.out.append("Area of Rectangle="+a.rectangleArea);
+System.out.println("\n Area of Triangle="+a.triangleArea);
+	}
+}
