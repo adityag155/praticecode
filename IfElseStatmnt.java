26006ba890a41141cdd16cabdf00d359c021641e
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-12 22:56:27 +0530

    Add files via upload

---
+++ IfElseStatmnt.java
@@ -0,0 +1,23 @@
+package adiiapplication;
+
+public class IfElseStatmnt {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		int b=20;
+		if(a+b<40)
+		{
+			System.out.println("a+b is less than 40");
+		}
+		else
+		{
+			System.out.println("a+b is greater than 40");
+		}
+	}
+}
+
+/*
+O/P:
+a+b is less than 40
+*/
