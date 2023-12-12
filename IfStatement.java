b3b367a31d903103721cb0caf24b1ec3033e7650
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-12 22:44:59 +0530

    Add files via upload

---
+++ IfStatement.java
@@ -0,0 +1,19 @@
+package adiiapplication;
+
+public class IfStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a=10;
+int b=20;
+if(a+b<50) {
+	System.out.println("a+b is less than 50");
+}
+	}
+
+}
+
+/*
+O/P:
+a+b is less than 50
+*/
