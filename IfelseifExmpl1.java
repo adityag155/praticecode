b9088436b79aebb2ecb9ccb9f0024d9c74c4b57f
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-13 20:21:28 +0530

    Add files via upload

---
+++ IfelseifExmpl1.java
@@ -0,0 +1,30 @@
+package adiiapplication;
+
+public class IfelseifExmpl1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1=5;
+		int num2=10;
+		
+		if(num1>num2)
+		{
+			System.out.println(num1+ "is greater than"+num2);
+		}
+		else if (num1<num2)
+		{
+			System.out.println(num1+"is less than"+num2);
+		}
+		else
+		{
+			System.out.println(num1+"is equal to"+num2);
+		}
+	}
+
+}
+
+/*
+O/P:
+5is less than10
+*/
