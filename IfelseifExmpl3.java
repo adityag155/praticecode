82ea1147712f73eb8cd266139c88a3d62949a1c6
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-13 20:22:08 +0530

    Add files via upload

---
+++ IfelseifExmpl3.java
@@ -0,0 +1,31 @@
+package adiiapplication;
+
+public class IfelseifExmpl3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String city="Delhi";
+		
+		if(city=="Pune") {
+			System.out.println("City is Pune");
+		}
+		else if(city=="Mumbai")
+		{
+			System.out.println("City is Mumbai");
+		}
+		else if(city=="Kolkata")
+		{
+			System.out.println("City is Kolkata");
+		}
+		else {
+			System.out.println(city);
+		}
+	}
+
+}
+
+/*
+ * O/P:
+ * Delhi
+ **/
