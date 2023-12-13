25e87d6087b2194c7e6e0eb0adc3c3bd15cdaa48
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-13 20:39:01 +0530

    Add files via upload

---
+++ IfelseifExmpl4.java
@@ -0,0 +1,30 @@
+package adiiapplication;
+import java.util.*;
+public class IfelseifExmpl4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int num1;
+int num2;
+
+Scanner sc=new Scanner(System.in);
+
+System.out.print("Enter the first number: ");
+num1 = sc.nextInt();
+
+System.out.print("Enter the second number: ");
+num2 = sc.nextInt();
+
+if(num1>num2)
+{
+	System.out.println(num1+"is greater than"+num2);
+}
+else if(num1<num2)
+{
+	System.out.println(num1+"is less than"+num2);
+}
+else {
+	System.out.println(num1+"both number are equal"+num2);
+}
+}
+}
