56e6d5abeee6a7ad068feb1a85609ffaf893a017
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-15 10:24:30 +0530

    Add files via upload

---
+++ MethodOverridingEx.java
@@ -0,0 +1,32 @@
+package adiiapplication;
+
+class First
+{
+	void meth()
+	{
+		System.out.println("Meth defined in 1st class");
+	}
+}
+
+class Second extends First
+{
+	void meth() {
+		System.out.println("Meth define in second class");
+	}
+	void access()
+	{
+		super.meth();
+	}
+}
+
+
+public class MethodOverridingEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Second s=new Second();
+		s.meth();
+		s.access();
+	}
+}
