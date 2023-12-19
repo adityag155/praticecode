9f6c7d3844ee4aa3731bacfb425e476de6a7f302
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-19 10:15:40 +0530

    Add files via upload

---
+++ IfelseIfex.java
@@ -0,0 +1,18 @@
+class IfelseIfex{
+public static void main(String args[]){
+int number=-13;
+if(number>0){
+System.out.println("Positive number");
+}else if(number<0){
+System.out.println("Negative number");
+}
+else{
+System.out.println("Zero");
+}
+}
+}
+
+/*
+O/P:
+Negative number
+*/
