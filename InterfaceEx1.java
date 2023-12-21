6414d06782837a2f02437b92e29e30409b03b5d3
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-21 22:40:23 +0530

    Add files via upload

---
+++ InterfaceEx1.java
@@ -0,0 +1,21 @@
+package adiiapplication;
+
+interface MyInter{
+	int VAR_NUM=200;
+}
+class MyClass{}
+
+public class InterfaceEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println(MyInter.VAR_NUM);
+		}
+
+}
+
+*/
+O/P:
+200
+*/
