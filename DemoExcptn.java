1902a0fabd333f1aaa3cc632a5c80652f3ed169f
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-23 22:02:48 +0530

    Add files via upload

---
+++ DemoExcptn.java
@@ -0,0 +1,14 @@
+class DemoExcptn{
+public static void main(String args[]){
+{
+int m=100, n=0;
+int k=m/n;
+System.out.println("This will be not printed");
+}
+}
+}
+
+/*
+O/P:
+This will be not printed
+*/
