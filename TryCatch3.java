0b468af4b0547e5e5c99675ed9b63ab2c029907b
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-23 23:31:07 +0530

    Add files via upload

---
+++ TryCatch3.java
@@ -0,0 +1,42 @@
+public class TryCatch3
+{
+
+    public static void main(String[] args)
+ {
+
+        try
+ {
+            // Code that may throw an exception
+
+            int result = divideNumbers(10, 0);
+
+            System.out.println("Result: " + result);
+ // This line won't be executed if an exception occurs
+
+        }
+
+ catch (ArithmeticException e)
+ {
+
+            // Handling the specific exception (ArithmeticException in this case)
+            System.out.println("An error occurred: " + e.getMessage());
+
+        }
+
+    }
+
+
+    public static int divideNumbers(int dividend, int divisor)
+ {
+
+        return dividend / divisor;
+ // This can throw an ArithmeticException if divisor is 0
+ 
+   }
+
+}
+
+/*
+O/P:
+An error occurred: / by zero
+*/
