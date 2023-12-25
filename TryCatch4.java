43e87d4a84624486b1345fb9bd5c7dab9dddbe80
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-25 12:11:12 +0530

    Add files via upload

--- TryCatch4.java
+++ TryCatch4.java
@@ -1,18 +1,45 @@
-class TryCatch4 {
-    public static void main(String args[]) {
-        try {
-            System.out.println("Start of inner try");
-            int m = 100;
-            int n = Integer.parseInt(args[0]);
-            int k = m / n;
-            System.out.println("K=" + k);
-            System.out.println("End of inner try");
-        } catch (ArithmeticException e) {
-            System.out.println("Can't divide by zero");
-        } catch (ArrayIndexOutOfBoundsException ae) {
-            System.out.println("Outer catch");
-        } finally {
-            System.out.println("End of outer try");
-        }
-    }
+package adiiapplication;
+
+//Multiple try block:
+
+public class TryCatch4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		try
+		{
+			System.out.println("Start of outer try");
+			
+			try
+			{
+				System.out.println("Start of inner try");
+				int m=100, n=Integer.parseInt(args[0]);
+				int k=m/n;
+				System.out.println("K="+k);
+				
+				System.out.println("End of inner try");
+			}
+			
+			catch(ArithmeticException e)
+			{
+				System.out.println("Cant divided by zero");
+			}
+			System.out.println("End of outer try");
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println("Invalid array index");
+		}
+		System.out.println("After catch");
+	}
+
 }
+
+/*
+O/P:
+Start of outer try
+Start of inner try
+Invalid array index
+After catch
+*/
