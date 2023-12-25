9db8dd3a61b65387c80ade341b8b669c7d1a41d0
adityag155 <100916704+adityag155@users.noreply.github.com>
2023-12-25 11:35:42 +0530

    Add files via upload

---
+++ TryCatch1.java
@@ -0,0 +1,16 @@
+package adiiapplication;
+
+public class TryCatch1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int m=100,  n=0;
+int k=m/n;
+System.out.println("This will be not printed");
+	}
+
+}
+
+/*
+java.lang.ArithmeticException: / by zero
+*/
