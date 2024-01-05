8974617cbd107929344f80ccfdc0d3e2d96fdb72
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-01-05 19:25:05 +0530

    Add files via upload

--- ThreadCreate.java
+++ ThreadCreate.java
@@ -1,52 +1,52 @@
+package adiiapplication;
+
 class MyThread implements Runnable
 {
-Thread t;
+    Thread t;
 
-MyThread(String tname)
-{
-t=new Thread(this,tname);
-t.start();
-}
+    MyThread(String tname)
+    {
+        t = new Thread(this, tname);
+        t.start();
+    }
 
-public void run()
-{
-for(int i=1;i<=4;i++)
-{
-System.out.println(t.getName()+":"+i);
+    public void run() {
+        for (int i = 1; i <= 4; i++) {
+            System.out.println(t.getName() + ":" + i);
 
-try
-{
-Thread.sleep(1000);
-}
-catch(InterruptedException ie)
-{}
-}
-}
+            try {
+                Thread.sleep(1000); // Reduced sleep time for demonstration purposes
+            } catch (InterruptedException ie) {
+                System.out.println(ie);
+            }
+        }
+    }
 }
 
-class ThreadCreate
-{
-public static void main(String args[])
-{
-new MyThread("One");
-new MyThread("Two");
-new MyThread("Three");
-}
-}
 
+
+public class ThreadCreate {
+
+    public static void main(String[] args) {
+        // Creating instances of MyThread
+        new MyThread("One");
+        new MyThread("Two");
+        new MyThread("Three");
+    }
+}
 
 /*
 O/P:
+Two:1
 One:1
 Three:1
-Two:1
 Two:2
-One:2
 Three:2
-One:3
+One:2
 Two:3
+One:3
 Three:3
 Two:4
-One:4
 Three:4
+One:4
 */
