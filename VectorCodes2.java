c794b3af0deeda7e31b4bfca7ea8124e0c3d559d
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-03 23:05:02 +0530

    Add files via upload

--- VectorCodes2.java
+++ VectorCodes2.java
@@ -21,18 +21,30 @@ System.out.println(v.capacity());
 System.out.println(v.size());
 
 v.add("One");
-v.add("Two");		//Here Capacity=3
-v.add("Three");		//AND Size=3
+v.add("Two");			//Here Capacity=3
+v.add("Three");			//AND Size=3
 
-System.out.println(v);
+System.out.println(v);			//Here will me vector printed=[One, Two, Three]
 
 System.out.println(v.capacity());
 System.out.println(v.size());
 
-v.add("Four");		//Size=4
+v.add("Four");			//Size=4
 System.out.println(v.capacity());     //Capacity will calculate how many compartments	=5
 System.out.println(v.size());	      //Size will calculate how many compartment filled
 
-System.out.println(v);
+System.out.println(v);			//Here will me vector printed=[One, Two, Three, Four]
 }
 }
+
+/*
+O/P
+3
+0
+[One, Two, Three]
+3
+3
+5
+4
+[One, Two, Three, Four]
+*/
