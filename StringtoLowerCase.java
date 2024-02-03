089cdab34fd57f2344f08e1fc36d2b08277431e3
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-03 12:29:35 +0530

    Add files via upload

---
+++ StringtoLowerCase.java
@@ -0,0 +1,20 @@
+class StringtoLowerCase
+{
+public static void main(String args[])
+{
+String str1="CPC";
+String str2="CPC";
+String str3="DPC";
+String str4="Hello";
+String str5="India is my Country. India is Great Nation";
+
+System.out.println(str1.toLowerCase());
+System.out.println(str5.toLowerCase());
+}
+}
+
+/*
+O/P:
+cpc
+india is my country. india is great nation
+*/
