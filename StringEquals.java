ab40ac90c9865ea704fd698c185a6619f58efc73
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-03 10:53:13 +0530

    Add files via upload

---
+++ StringEquals.java
@@ -0,0 +1,20 @@
+class StringEquals
+{
+public static void main(String args[])
+{
+String str1="CPC";
+String str2="CPC";
+String str3="DPC";
+String str4="Hello";
+String str5="India is my country.India is great nation";
+
+System.out.println(str1.equals(str2));
+System.out.println(str1.equals(str4));
+}
+}
+
+/*
+O/P:
+true
+false
+*/
