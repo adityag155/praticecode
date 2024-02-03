12f7e03b463f77271f79fa5c22ac907e7cb6fd55
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-03 12:24:04 +0530

    Add files via upload

---
+++ StringtoUpperCase.java
@@ -0,0 +1,19 @@
+class StringtoUpperCase
+{
+public static void main(String args[])
+{
+String str1="CPC";
+String str2="CPC";
+String str3="DPC";
+String str4="Hello";
+String str5="India is my country. Indai is great nation";
+
+System.out.println(str4.toUpperCase());
+System.out.println(str5.toUpperCase());
+}
+}
+
+/*
+HELLO
+INDIA IS MY COUNTRY. INDAI IS GREAT NATION
+*/
