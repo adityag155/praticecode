dcd8804202e032497de6fcea6af4faf069de682a
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-03 09:59:33 +0530

    Add files via upload

---
+++ ExampleofS.java
@@ -0,0 +1,26 @@
+class ExampleofS
+{
+public static void main(String args[])
+{
+String str1="CPC";	//3
+String str2="CPC";	//3
+String str3="DPC";	//3
+String str4="Hello";	//5
+String str5="India is my country.India is great nation";	//41
+
+System.out.println(str1.length());
+System.out.println(str2.length());
+System.out.println(str3.length());
+System.out.println(str4.length());
+System.out.println(str5.length());
+}
+}
+
+/*
+O/P:
+3
+3
+3
+5
+41
+*/
