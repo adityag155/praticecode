96e1c937807305c01bfc46b9bb3f2cec88bebfc3
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-03 10:14:42 +0530

    Add files via upload

---
+++ StringCharAt.java
@@ -0,0 +1,26 @@
+class StringCharAt
+{
+public static void main(String args[])
+{
+String str1="CPC";
+String str2="CPC";
+String str3="DPC";
+String str4="Hello";
+String str5="India is my Country, India is great nation";
+
+System.out.println(str1.charAt(1));
+System.out.println(str2.charAt(2));
+System.out.println(str3.charAt(0));
+System.out.println(str4.charAt(4));
+System.out.println(str5.charAt(7));
+}
+}
+
+/*
+O/P:
+P
+C
+D
+o
+s
+*/
