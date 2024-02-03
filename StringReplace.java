1ac0c94d12e4de992f4f504bd4dae94fa0671f59
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-03 13:09:28 +0530

    Add files via upload

---
+++ StringReplace.java
@@ -0,0 +1,20 @@
+class StringReplace
+{
+public static void main(String args[])
+{
+String str1="CPC";
+String str2="CPC";
+String str3="DPC";
+String str4="Hello";
+String str5="India is my country. India is great nation";
+
+System.out.println(str4.replace('l','m'));
+System.out.println(str2.replace('C','G'));
+}
+}
+
+/*
+O/P:
+Hemmo
+GPG
+*/
