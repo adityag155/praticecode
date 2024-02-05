8561b1990ab8b639c1811533efc1806a21260837
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-05 23:04:21 +0530

    Add files via upload

--- InstanceOperator.java
+++ InstanceOperator.java
@@ -1,6 +1,8 @@
-class First{}
+ class First{}
 
 class Second extends First{}
+
+class Third extends Second{}
 
 public class InstanceOperator {
 
@@ -10,9 +12,17 @@ public class InstanceOperator {
 		First f=new First();
 		Second s=new Second();
 		Third t=new Third();
-		System.out.println(f instanceof First);
-		System.out.println(f instanceof Second);
-		System.out.println(f instanceof Third);
+		System.out.println(f instanceof First);		//t
+		System.out.println(f instanceof Second);	//f
+		System.out.println(f instanceof Third);		//f
+
+		System.out.println(s instanceof First);		//t
+		System.out.println(s instanceof Second);	//t
+		System.out.println(s instanceof Third);		//f
+
+		System.out.println(t instanceof First);		//t
+		System.out.println(t instanceof Second);	//t
+		System.out.println(t instanceof Third);		//t
 		
 	
 	}
@@ -24,4 +34,12 @@ O/P:
 true
 false
 false
+
+true
+true
+false
+
+true
+true
+true
 */
