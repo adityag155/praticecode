861b1ec799cd6ea511b22a8076879d339aed66ec
adityag155 <100916704+adityag155@users.noreply.github.com>
2024-02-13 11:25:11 +0530

    Add files via upload

---
+++ WrestlerDemoEx.java
@@ -0,0 +1,65 @@
+import java.util.*;
+class Wrestler
+{
+private String wname;
+private int weight;
+Wrestler(String wname,int weight)
+{
+this.wname=wname;
+this.weight=weight;
+}
+public String getWname()
+{
+return wname;
+}
+public int getWeight()
+{
+return weight;
+}
+public String toString()
+{
+return wname+" "+weight;
+}
+
+public boolean equals(Object obj)
+{
+if(obj instanceof Wrestler)
+{
+if(((Wrestler)obj).getWname().equals(this.getWname())&&(((Wrestler)obj).getWeight()==this.getWeight()))
+return true;
+else
+return false;
+}
+return false;
+}
+
+public int hashCoode()
+{
+if(weight>=120)
+return 1;
+else
+return 0;
+}
+}
+
+class WrestlerDemoEx
+{
+public static void main(String args[])
+{
+Wrestler obj=new Wrestler("Satish",125);
+
+HashSet<Wrestler>set=new HashSet<Wrestler>();
+set.add(new Wrestler("Satish",125));
+set.add(new Wrestler("Anil",120));
+set.add(new Wrestler("Uma",70));
+set.add(new Wrestler("Saket",100));
+set.add(new Wrestler("Vijay",110));
+
+System.out.println(set.contains(obj));
+}
+}
+/*
+O/P:
+false
+*/
+
